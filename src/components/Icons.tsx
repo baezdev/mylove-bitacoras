bbf9d5fe89507ed7fd075ced364af037860a24d7
baezdev <baezdev@gmail.com>
2023-05-30 00:41:19 -0600

    add svg icons

---
+++ src/components/Icons.tsx
@@ -0,0 +1,47 @@
+export function AddIcon () {
+  return (
+    <svg width='20px' height='20px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M4 12H9M12 12H20M12 4V20' stroke='#000000' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
+
+export function BitacoraIcon () {
+  return (
+    <svg width='20px' height='20px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M15 12L10.8 16.2L9.00003 14.4M9 4.5V6H15V4.5M9 4.5V3H15V4.5M9 4.5H5V14M15 4.5H19V8M19 13V21H5V18' stroke='#000000' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
+
+export function CloseIcon () {
+  return (
+    <svg width='30px' height='30px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M19 5L5 19M5 5L9.5 9.5M12 12L19 19' stroke='#000000' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
+
+export function MenuIcon () {
+  return (
+    <svg width='30px' height='30px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M4 17H8M12 17H20M4 12H20M4 7H12M16 7H20' stroke='#000000' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
+
+export function SaveIcon () {
+  return (
+    <svg width='20px' height='20px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M4 13V18C4 19.1046 4.89543 20 6 20H18C19.1046 20 20 19.1046 20 18V16M4 8V6C4 4.89543 4.89543 4 6 4H14.1716C14.702 4 15.2107 4.21071 15.5858 4.58579L19.4142 8.41421C19.7893 8.78929 20 9.29799 20 9.82843V12M15 20V15H9V20' stroke='#fff' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
+
+export function TrashIcon () {
+  return (
+    <svg width='30px' height='30px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M10 10V16M14 10V16M4 6H20M15 6V5C15 3.89543 14.1046 3 13 3H11C9.89543 3 9 3.89543 9 5V6M18 6V14M18 18C18 19.1046 17.1046 20 16 20H8C6.89543 20 6 19.1046 6 18V13M6 9V6' stroke='#ffffff' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
