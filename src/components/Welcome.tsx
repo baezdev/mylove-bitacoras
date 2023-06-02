3b2d24abdfa9f8cd94cc1dcc5482485fe657a2a9
baezdev <baezdev@gmail.com>
2023-06-02 00:57:30 -0600

    add section that welcome

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,8 @@
+export const Welcome = () => {
+  return (
+    <section className='py-2 md:py-10 basis-1/2'>
+      <h2 className='text-2xl font-semibold md:text-4xl'>Bienvenida a</h2>
+      <h3 className='flex justify-start gap-2 text-4xl font-semibold md:text-6xl'><span className='font-black text-pink-400'>MyLove</span> Bitacoras</h3>
+    </section>
+  )
+}
