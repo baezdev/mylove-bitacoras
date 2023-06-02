f1f0810f6a6b7e9da61d0a811ad5616569010a73
baezdev <baezdev@gmail.com>
2023-06-02 00:57:16 -0600

    add index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import { Form } from "@/components/Form";
+import { Welcome } from "@/components/Welcome";
 
 export default function Home() {
   return (
-    <h1>BUenas</h1>
+    <main className='flex flex-col w-11/12 mx-auto md:flex-row md:max-w-screen-xl'>
+      <Welcome />
+      <Form />
+    </main>
   )
 }
